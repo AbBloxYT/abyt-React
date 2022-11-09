85031db00b5f6b7335e4cbbdf3c4c6da4248f921
AbBloxYT <116005754+AbBloxYT@users.noreply.github.com>
2022-11-09 12:18:34 -0500

    Commit from TeleportHQ

--- src/views/games.js
+++ src/views/games.js
@@ -20,122 +20,77 @@ const Games = (props) => {
         />
       </Helmet>
       <div id="games" className="games-features">
-        <div className="games-container1"></div>
-        <div className="games-container2">
+        <div className="games-container01"></div>
+        <div className="games-container02">
           <span className="games-text">
-            Here are some games of mine (credit to guzuv for the games!)Click
-            the image above text to enter game
+            Yo Peeps I just updated with new Games and more HAVE FUN PEEPS!
           </span>
           <h2>
             <br></br>
             <br></br>
           </h2>
         </div>
+        <div className="games-container03">
+          <h5 className="games-text04">Mario 64</h5>
+          <span className="games-text05">
+            <span>Mario 64 really fun</span>
+            <br></br>
+            <br></br>
+          </span>
+        </div>
         <div className="games-features1">
-          <div className="games-container3">
-            <a
-              href="https://28klotlucas2.github.io/Mario64webgl/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="games-link"
-            >
-              <img
-                alt="image"
-                src="/playground_assets/01.svg"
-                className="games-image"
-              />
-            </a>
-            <h5 className="games-text04 Headline5">Mario 64</h5>
-            <span className="games-text05">
-              <span>Mario 64 really fun</span>
-              <br></br>
-              <br></br>
-            </span>
+          <div className="games-container04">
+            <div className="games-container05">
+              <h5 className="games-text09">Coming soon</h5>
+              <span className="games-text10">
+                <span>Just wait this game still working</span>
+                <br></br>
+                <span>on it.</span>
+              </span>
+            </div>
+            <div className="games-container06">
+              <h5 className="games-text14">
+                <span className="games-text15">Among</span>
+                <span className="Headline5">
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span className="games-text17">us</span>
+              </h5>
+              <span className="games-text18">
+                I present amongus! Like among us? Then try this to all fans.
+              </span>
+            </div>
           </div>
-          <div className="games-container4">
-            <a
-              href="https://guzuv.github.io/guzEmu/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="games-link1"
-            >
-              <img
-                alt="image"
-                src="/playground_assets/04.svg"
-                className="games-image1"
-              />
-            </a>
-            <h5 className="games-text09 Headline5">G.B.A.</h5>
-            <span className="games-text10">
+          <div className="games-container07">
+            <h5 className="games-text19">G.B.A.</h5>
+            <span className="games-text20">
               <span>Game boy advanced</span>
               <br></br>
               <span>with games</span>
               <br></br>
             </span>
           </div>
-          <div className="games-container5">
-            <img
-              alt="image"
-              src="/playground_assets/03.svg"
-              className="games-image2"
-            />
-            <h5 className="games-text15 Headline5">Coming soon</h5>
-            <span className="games-text16">
-              <span>Just wait this game still working</span>
-              <br></br>
-              <span>on it.</span>
-            </span>
-          </div>
-          <div className="games-container6">
-            <a
-              href="https://among-us-online-fan-remake.1tim.repl.co/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="games-link2"
-            >
-              <img
-                alt="Among us"
-                src="/playground_assets/02.svg"
-                className="games-image3"
-              />
-            </a>
-            <h5 className="games-text20 Headline5">Among us</h5>
-            <span className="games-text21">
-              I present amongus! Like among us? Then try this to all fans.
-            </span>
-          </div>
-          <div className="games-container7">
-            <a
-              href="https://hyper-network.me/"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="games-link3"
-            >
-              <img
-                alt="image"
-                src="/playground_assets/05.svg"
-                className="games-image4"
-              />
-            </a>
-            <h5 className="games-text22 Headline5">Minecraft</h5>
-            <span className="games-text23">
-              <span>Cant play minecraft then try this</span>
-              <br></br>
-              <br></br>
-            </span>
-          </div>
-          <div className="games-container8">
-            <img
-              alt="image"
-              src="/playground_assets/06.svg"
-              className="games-image5"
-            />
-            <h5 className="games-text27 Headline5">Coming soon</h5>
-            <span className="games-text28">
+          <div className="games-container08">
+            <h5 className="games-text25">Coming soon</h5>
+            <span className="games-text26">
               <span>is gonna be a good game</span>
               <br></br>
             </span>
           </div>
+        </div>
+        <div className="games-container09">
+          <h5 className="games-text29">Minecraft</h5>
+          <span className="games-text30">
+            <span className="games-text31">
+              Cant play minecraft then try this
+            </span>
+            <br className="games-text32"></br>
+            <br></br>
+          </span>
         </div>
       </div>
     </div>
