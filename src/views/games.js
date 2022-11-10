f62b2d35dde792f5a19179084ede42d7dd395f55
AbBloxYT <116005754+AbBloxYT@users.noreply.github.com>
2022-11-09 19:58:38 -0500

    Commit from TeleportHQ

--- src/views/games.js
+++ src/views/games.js
@@ -22,16 +22,21 @@ const Games = (props) => {
       <div id="games" className="games-features">
         <div className="games-container01"></div>
         <div className="games-container02">
-          <span className="games-text">
-            Yo Peeps I just updated with new Games and more HAVE FUN PEEPS!
-          </span>
           <h2>
             <br></br>
             <br></br>
           </h2>
         </div>
+        <span className="games-text03">
+          Yo Peeps I just updated with new Games and more HAVE FUN PEEPS!
+        </span>
+        <img
+          src="/playground_assets/02.svg"
+          alt="image"
+          className="games-image"
+        />
         <div className="games-container03">
-          <h5 className="games-text04">Mario 64</h5>
+          <h5 className="games-text04 Headline5">Mario 64</h5>
           <span className="games-text05">
             <span>Mario 64 really fun</span>
             <br></br>
@@ -41,42 +46,28 @@ const Games = (props) => {
         <div className="games-features1">
           <div className="games-container04">
             <div className="games-container05">
-              <h5 className="games-text09">Coming soon</h5>
+              <h5 className="games-text09 Headline5">Coming soon</h5>
               <span className="games-text10">
-                <span>
-                  Just wait this game
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-                <span>still</span>
-                <span> working</span>
+                <span>Just wait this game still working</span>
                 <br></br>
                 <span>on it.</span>
               </span>
             </div>
             <div className="games-container06">
-              <h5 className="games-text16">
-                <span className="games-text17">Among</span>
-                <span className="Headline5">
-                  <span
-                    dangerouslySetInnerHTML={{
-                      __html: ' ',
-                    }}
-                  />
-                </span>
-                <span className="games-text19">us</span>
-              </h5>
-              <span className="games-text20">
+              <img
+                src="/playground_assets/01.svg"
+                alt="image"
+                className="games-image1"
+              />
+              <h5 className="games-text14 Headline5">Among us</h5>
+              <span className="games-text15">
                 I present amongus! Like among us? Then try this to all fans.
               </span>
             </div>
           </div>
           <div className="games-container07">
-            <h5 className="games-text21">G.B.A.</h5>
-            <span className="games-text22">
+            <h5 className="games-text16 Headline5">G.B.A.</h5>
+            <span className="games-text17">
               <span>Game boy advanced</span>
               <br></br>
               <span>with games</span>
@@ -84,23 +75,26 @@ const Games = (props) => {
             </span>
           </div>
           <div className="games-container08">
-            <h5 className="games-text27">Coming soon</h5>
-            <span className="games-text28">
+            <h5 className="games-text22 Headline5">Coming soon</h5>
+            <span className="games-text23">
               <span>is gonna be a good game</span>
               <br></br>
             </span>
           </div>
         </div>
         <div className="games-container09">
-          <h5 className="games-text31">Minecraft</h5>
-          <span className="games-text32">
-            <span className="games-text33">
-              Cant play minecraft then try this
-            </span>
-            <br className="games-text34"></br>
+          <h5 className="games-text26 Headline5">Minecraft</h5>
+          <span className="games-text27">
+            <span>Cant play minecraft then try this</span>
+            <br></br>
             <br></br>
           </span>
         </div>
+        <img
+          src="/playground_assets/03.svg"
+          alt="image"
+          className="games-image2"
+        />
       </div>
     </div>
   )
